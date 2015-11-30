92fbf4a6d0dfcb372b3198c36deff56d8bc58239
TurplePurtle <santi256@gmail.com>
2015-11-29 18:05:23 -0600

    fix xboxone axes mapping

--- src/lib/gamepad-mapper.js
+++ src/lib/gamepad-mapper.js
@@ -2,7 +2,7 @@ const mappings = {
   XBOXONE: {
     standard: {
       buttons: [11, 12, 13, 14, 8, 9, -1, -1, 5, 4, 6, 7, 0, 1, 2, 3, 10],
-      axes: [0, 1, 2, 3],
+      axes: [0, 1, 3, 4],
     },
   },
 };
