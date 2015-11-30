cbe9afd30b237d33992b07fdfc8a56a5234707a6
TurplePurtle <santi256@gmail.com>
2015-11-29 20:45:35 -0600

    remove commented code

--- src/index.js
+++ src/index.js
@@ -8,12 +8,6 @@ input.addEventListeners();
 
 const scene = new Demo(context, input).load().start();
 
-// const pause = e => { scene.stop(); };
-// const unpause = e => { scene.start(); };
-//
-// window.addEventListener("blur", pause);
-// window.addEventListener("focus", unpause);
-
 const autoPause = e => {
   if (document.hidden) {
     scene.stop();
